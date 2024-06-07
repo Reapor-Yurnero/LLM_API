e5045651a0198ed7b2f4063574d42ab3a8245dbd
Xiaohan Fu <reapor.yurnero@gmail.com>
2024-06-06 22:59:56 -0700

    exp scripts + data

---
+++ exp9_3.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=~/mod_propane/:$PYTHONPATH
+export CUDA_VISIBLE_DEVICES=0
+python experiments/run_experiments_hard.py --exp_name exp9 --model_name_or_path /data/models/hf/glm-4-9b-chat --dataset_path ./datasets/conversations_keywords_url_glm4.pkl  --output_dir ./results --batch_size 5 --top_k 250 --num_epochs 2000 --kl_every 1 --subset_size 25 --n_proposals 8 --optim_suffix_len 15 --clip_vocab --n_trials 1 --fp16 --start_from_scratch
